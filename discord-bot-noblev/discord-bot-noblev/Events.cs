4d81b3a2b2daaed2dc24a7ac058781da38ba1116
Justus Braun <justusbraun03@gmail.com>
2021-05-26 00:16:44 +0200

    First real Init

---
+++ discord-bot-noblev/discord-bot-noblev/Events.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace discord_bot_noblev
+{
+    public static class Events
+    {
+        public static async Task MessageReceived(SocketMessage message)
+        {
+            //If the Author is a bot it leaves method
+            if (message.Author.IsBot) { return; }
+            
+            //If the Message starts with the set Prefix
+            if (message.Content.StartsWith("!"))
+            {
+                switch (message.Content.Remove(0, 1).Split(' ')[0])
+                {
+                    case "help":
+                        await Log(new LogMessage(LogSeverity.Info, "help", $"Help by {message.Author.Mention}"));
+
+                        var embed = new EmbedBuilder()
+                        {
+                            Title = "Test",
+                            Description = "test"
+                        };
+
+                        await message.Channel.SendMessageAsync("", false, embed.Build());
+                        break;
+                }
+            }
+        }
+
+        //Checks if User is admin on guild
+        private static bool IsAuthorAdmin(SocketMessage message) => ((SocketGuildUser)message.Author).GuildPermissions.Administrator;
+
+        //This method is used to write log
+        public static Task Log(LogMessage msg)
+        {
+            Console.Write($"{DateTime.Now.ToString(CultureInfo.CurrentCulture).Remove(0, 11)} {msg.Severity} {msg.Source} {msg.Exception}");
+            Console.SetCursorPosition(40, Console.CursorTop);
+            Console.WriteLine(msg.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ discord-bot-noblev/discord-bot-noblev/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json.Linq;
+
+namespace discord_bot_noblev
+{
+    class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public static DiscordSocketClient Client;
+        
+        public static readonly string Json = File.ReadAllText(@"..\..\..\..\..\config.json");
+
+        private async Task MainAsync()
+        {
+            //Client Events
+            Client = new DiscordSocketClient();
+            Client.MessageReceived += Events.MessageReceived;
+
+            //Gets Token from Json
+            var token = JObject.Parse(Json)["api"]["token"].ToString();
+
+            //Logging in 
+            await Client.LoginAsync(TokenType.Bot, token);
+            await Client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+    }
+}
